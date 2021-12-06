4f4d00af8e2ba76469ebab61172c8c1abc6ba311
Tomás Jiménez <tjimenez999@gmail.com>
2021-12-05 22:24:31 -0300

    improve accesibility

--- components/ToggleThemeButton.tsx
+++ components/ToggleThemeButton.tsx
@@ -5,6 +5,7 @@ const ToggleThemeButton: React.FC = () => {
   const [theme, toggleTheme] = useDarkMode();
   return (
     <Button
+      ariaLabel="Botón de cambio de tema"
       onClick={toggleTheme}
       className="text-white font-bold p-2 rounded bg-gray-800 dark:bg-purple-600"
     >
